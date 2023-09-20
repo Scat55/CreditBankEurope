c97332ae7e4d61908102c8ac911aadb5be7d883a
Dmitry Bystrov <dmit.bystroff2106@yandex.ru>
2023-09-21 00:09:21 +0300

    fix: fixed main section

---
+++ src/components/premium.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="premium">
+    <div class="premium__text">
+      <h1 class="premium__text-title">
+        Твоя премиум карта
+        <span class="premium__text-subtitle">Лучшая карта для любителей путешествий</span>
+      </h1>
+      <img
+        src="../assets/images/card.png"
+        alt="CreditCard"
+        class="premium__text-img"
+      />
+    </div>
+    <div class="premium__conditions">
+      <div class="premium__conditions-miles">
+        <span class="premium__conditions-miles-orange orange">11%</span>
+        <p class="premium__conditions-miles-title dark_span">Вернем за покупки милями</p>
+      </div>
+      <div class="premium__conditions-cash">
+        <span class="premium__conditions-cash-orange orange">0%</span>
+        <p class="premium__conditions-cash-title dark_span">
+          Бесплатное снятие наличных по всему миру
+        </p>
+      </div>
+      <div class="premium__conditions-remains">
+        <span class="premium__conditions-remains-orange orange">7%</span>
+        <p class="premium__conditions-remains-title dark_span">До 7% на остаток по счету</p>
+      </div>
+      <div class="premium__conditions-free">
+        <span class="premium__conditions-free-orange orange">0р</span>
+        <p class="premium__conditions-free-title dark_span">Бесплатное годовое обслуживание</p>
+      </div>
+    </div>
+
+    <router-link to="/form"><button class="premium__btn">Заказать сейчас</button></router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/vars.scss';
+
+.premium {
+  margin-top: 100px;
+  margin-left: 27.25rem;
+
+  &__text {
+    position: relative;
+    display: block;
+    width: 735px;
+
+    &-title {
+      display: block;
+      color: $textColor;
+      font-family: Geometria;
+      font-size: 96px;
+      font-style: normal;
+      font-weight: 900;
+      line-height: normal;
+    }
+
+    &-subtitle {
+      display: inline-block;
+      color: $spanColor;
+      font-family: Geometria;
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 800;
+      line-height: normal;
+      width: 350px;
+      height: 80px;
+    }
+
+    &-img {
+      position: absolute;
+      top: 130px;
+      right: -345px;
+      width: 100%;
+
+      @media (min-width: 1820px) and (max-width: 1920px) {
+        top: -16px;
+        right: -748px;
+        width: 100%;
+      }
+    }
+  }
+
+  &__conditions {
+    display: grid;
+    grid-template: repeat(2, 100px) / repeat(2, 150px);
+    margin-top: 84px;
+    gap: 40px;
+
+    &-miles {
+      &-title {
+        width: 110px;
+      }
+    }
+
+    &-cash {
+      &-title {
+        width: 127px;
+      }
+    }
+
+    &-remains {
+      &-title {
+        width: 60px;
+      }
+    }
+
+    &-free {
+      &-title {
+        width: 100px;
+      }
+    }
+  }
+
+  &__btn {
+    width: 300px;
+    height: 80px;
+    border-radius: 10px;
+    background: #c0965c;
+    padding: 28px 47px 24px 44px;
+    outline: none;
+    color: #fff;
+    text-align: center;
+    font-family: Geometria;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 113.22%;
+    cursor: pointer;
+    margin-top: 97px;
+  }
+}
+
+.orange {
+  color: #c0965c;
+  font-family: Geometria;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
+.dark_span {
+  color: #6c6c6c;
+  font-family: Geometria;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,17 +8,7 @@
         <Header />
       </header>
       <main class="main">
-
-        <section class="premium">
-          <div class="premium__text">
-            <p class="premium__text-title">Твоя премиум
-              карта
-              <span class="premium__text-subtitle">Лучшая карта для
-                любителей путешествий</span>
-            </p>
-          </div>
-        </section>
-
+        <Premium />
       </main>
     </div>
 
@@ -29,10 +19,12 @@
 <script>
 import Header from '../components/header.vue';
 import Aside from '../components/aside.vue';
+import Premium from '../components/premium.vue';
 export default {
   components: {
     Header,
-    Aside
+    Aside,
+    Premium
   },
 }
 </script>
@@ -40,39 +32,5 @@ export default {
 <style lang="scss" scoped>
 .main {
   background: url('../assets/images/bg.svg') no-repeat;
-}
-
-.premium {
-  margin-top: 100px;
-  margin-left: 27.25rem;
-
-  &__text {
-    display: block;
-    width: 735px;
-
-
-    &-title {
-      display: block;
-      color: #FFF;
-      font-family: Geometria;
-      font-size: 96px;
-      font-style: normal;
-      font-weight: 900;
-      line-height: normal;
-
-    }
-
-    &-subtitle {
-      display: inline-block;
-      color: #6C6C6C;
-      font-family: Geometria;
-      font-size: 24px;
-      font-style: normal;
-      font-weight: 800;
-      line-height: normal;
-      width: 350px;
-      height: 80px;
-    }
-  }
 }
 </style>
