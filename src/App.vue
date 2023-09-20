dc7d1b9aad9ecfc667126bad16ef7bfdfebf3f37
Dmitry Bystrov <dmit.bystroff2106@yandex.ru>
2023-09-20 14:56:22 +0300

    init: start task

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="container">
+      <p>Твоя премиум карта</p>
+    </div>
   </div>
 </template>
 
@@ -13,20 +11,13 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+  p {
+    font-family: Geometria;
+    font-size: 96px;
+    font-style: normal;
+    font-weight: 800;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
 </style>
